fd2a5da761686a8d78ab365003fcfe8e7debe0c0
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-04-18 15:59:38 -0500

    Make functions work better.

--- textFilesToSpreadsheet.py
+++ textFilesToSpreadsheet.py
@@ -2,33 +2,36 @@
 # textFilesToSpreadsheet.py — An exercise in manipulating Excel files.
 # For more information, see project_details.txt.
 
-import os
+from pathlib import Path
+import logging
 import openpyxl
 
-import logging
-
-logging.basicConfig(level=logging.DEBUG, filename="logging.txt")
-
-document_contents = []
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="logging.txt",
+    format="%(asctime)s -  %(levelname)s -  %(message)s",
+)
+# logging.disable(logging.CRITICAL) # Remove notation to disable logging.
 
 
 def find_files():
-    user_input = input("Please type file path here: ")
-    directory = os.chdir(user_input)
-    document_list = os.listdir(directory)
-    text_files = []
-    for item in document_list:
-        text_files.append(f"{user_input}/{item}")
-    return text_files
+    """Find all txt files in designated folder."""
+    user_path = Path(input("Please type file path here: "))
+    document_list = list(user_path.glob("*.txt"))
+    logging.debug(document_list)
+    return document_list
 
 
-def pull_contents(doc_list):
-    for file in doc_list:
+def pull_contents(file_list):
+    """Convert contents of each line of each text file into a list of lists."""
+    document_contents = []
+    for file in file_list:
         with open(file, "rb") as f:
             lines = f.readlines()
             document_contents.append(lines)
+    return document_contents
 
 
-document_list = find_files()
-pull_contents(document_list)
-logging.debug(document_contents)
+doc_list = find_files()
+doc_contents = pull_contents(doc_list)
+logging.debug(doc_contents)
