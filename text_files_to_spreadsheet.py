d8f1d62911f96de356955758eb9f58faa32b6688
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-08-27 20:25:44 -0500

    Tighten up logic in write excel function.

--- text_files_to_spreadsheet.py
+++ text_files_to_spreadsheet.py
@@ -4,7 +4,7 @@
 
 from pathlib import Path
 import logging
-import openpyxl
+from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
 
 logging.basicConfig(
@@ -16,46 +16,7 @@ logging.basicConfig(
 logging.disable(logging.CRITICAL)  # Note out to enable logging.
 
 
-def find_files():
-    """Find all txt files in designated folder
-    and make a list of their file paths."""
-    user_path = Path(input("Please type file path here: "))
-    document_list = list(user_path.glob("*.txt"))
-    logging.debug(document_list)
-    return document_list
-
-
-def pull_contents(file_list):
-    """Convert contents of each line of each text file into a list of lists."""
-    document_contents = []
-    for item in file_list:
-        with open(item, "rb") as f_contents:
-            lines = f_contents.readlines()
-            document_contents.append(lines)
-    return document_contents
-
-
-def write_excel(contents):
-    """Create an Excel file with every txt files as its own column
-    and each line of the txt file as a row."""
-    wb = openpyxl.Workbook()
-    sheet = wb.active
-    column_number = len(contents)
-
-    for idx in range(column_number):
-        column_letter = get_column_letter(idx + 1)
-        logging.debug(column_letter)
-        row_number = len(contents[idx])
-
-        for item in range(row_number):
-            sheet_info = f"{column_letter}{item + 1}"
-            logging.debug(sheet_info)
-            sheet[f"{sheet_info}"] = contents[idx][item]
-
-    wb.save("text_to_spread.xlsx")
-
-
-def main_func():
+def main():
     """Executes prior functions in sequence."""
     doc_list = find_files()
     doc_contents = pull_contents(doc_list)
@@ -63,4 +24,33 @@ def main_func():
     write_excel(doc_contents)
 
 
-main_func()
+def find_files():
+    """Find all txt files in designated folder
+    and make a list of their file paths."""
+    return list(Path(input("Please type file path here: ")).glob("*.txt"))
+
+
+def pull_contents(file_list):
+    """Convert contents of each line of each text file into a list of lists."""
+    document_contents = []
+    for item in file_list:
+        with open(item, "rb") as file_contents:
+            document_contents.append(file_contents.readlines())
+    return document_contents
+
+
+def write_excel(contents):
+    """Create an Excel file with every txt files as its own column
+    and each line of the txt file as a row."""
+    wb = Workbook()
+    sheet = wb.active
+
+    for i, _ in enumerate(contents):
+        for j, _ in enumerate(contents[i]):
+            sheet[f"{get_column_letter(i + 1)}{j + 1}"] = contents[i][j]
+
+    wb.save("text_to_spread.xlsx")
+
+
+if __name__ == "__main__":
+    main()
